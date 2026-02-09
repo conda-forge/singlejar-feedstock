a10d75346de06f16590a2ea1b676b0de8ac3e6a3
Uwe L. Korn <uwe.korn@quantco.com>
2026-02-09 16:22:38 +0100

    Limit to old bazel toolchain

--- recipe/build.sh
+++ recipe/build.sh
@@ -19,7 +19,8 @@ sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/ijar/BUILD
 
 chmod +x bazel-${PKG_VERSION}
 pushd src/tools/singlejar
-../../../bazel-${PKG_VERSION} build --logging=6 --subcommands --verbose_failures --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include --crosstool_top=//bazel_toolchain:toolchain --cpu ${TARGET_CPU} singlejar singlejar_local
+../../../bazel-${PKG_VERSION} build --logging=6 --subcommands --verbose_failures --define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include --crosstool_top=//bazel_toolchain:toolchain --cpu ${TARGET_CPU} \
+	singlejar singlejar_local
 mkdir -p $PREFIX/bin
 cp ../../../bazel-out/${TARGET_CPU}-fastbuild/bin/src/tools/singlejar/singlejar $PREFIX/bin
 cp ../../../bazel-out/${TARGET_CPU}-fastbuild/bin/src/tools/singlejar/singlejar_local $PREFIX/bin
