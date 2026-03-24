785b5fff8051acd2d81bbef698ce1b2e5f1c7005
Uwe L. Korn <uwe.korn@quantco.com>
2026-03-24 13:38:49 +0100

    Use systemlib packages

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,22 +8,21 @@ if [[ "${target_platform}" == osx-* ]]; then
     export LDFLAGS="${LDFLAGS} -target ${HOST}"
 fi
 
+# Prepare systemlibs defintions
+mkdir -p third_party/systemlibs
+cp -ap "${PREFIX}/share/bazel/systemlibs/absl" third_party/systemlibs/
+cp -ap "${PREFIX}/share/bazel/systemlibs/protobuf" third_party/systemlibs/
+cp -ap $PREFIX/share/bazel/protobuf/bazel third_party/systemlibs/protobuf/
+
 source gen-bazel-toolchain
 
-export ABSEIL_VERSION=$(conda list -p $PREFIX libabseil --fields version | grep -v '#')
-export PROTOC_VERSION=$(conda list -p $PREFIX libprotobuf | grep -v '^#' | tr -s ' ' | cut -f 2 -d ' ' | sed -E 's/^[0-9]+\.([0-9]+\.[0-9]+)$/\1/')
+export ABSEIL_VERSION="$(conda list -p "${PREFIX}" libabseil --fields version | awk '!/^#/ && NF { print $1; exit }')"
+export PROTOC_VERSION="$(conda list -p "${PREFIX}" libprotobuf --fields version | awk '!/^#/ && NF { print $1; exit }' | sed -E 's/^[0-9]+\.([0-9]+\.[0-9]+)$/\1/')"
 export PROTOBUF_JAVA_MAJOR_VERSION="3"
 export EXTRA_BAZEL_ARGS="--host_javabase=@local_jdk//:jdk"
-sed -ie "s:\${INSTALL_NAME_TOOL}:${INSTALL_NAME_TOOL:-install_name_tool}:" src/BUILD
-sed -ie "s:\${PREFIX}:${PREFIX}:" src/BUILD
-sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/grpc/BUILD
-sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/systemlibs/protobuf/BUILD
-sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/systemlibs/protobuf/src/google/protobuf/compiler/BUILD
-sed -ie "s:\${BUILD_PREFIX}:${BUILD_PREFIX}:" third_party/ijar/BUILD
-sed -ie "s:ABSEIL_VERSION:${ABSEIL_VERSION}:" third_party/systemlibs/protobuf/MODULE.bazel
-sed -ie "s:ABSEIL_VERSION:${ABSEIL_VERSION}:" MODULE.bazel
-
-cp -ap $PREFIX/share/bazel/protobuf/bazel third_party/systemlibs/protobuf/
+sed -ie "s:ABSEIL_VERSION:${ABSEIL_VERSION}:" \
+	third_party/systemlibs/protobuf/MODULE.bazel \
+	MODULE.bazel
 
 chmod +x bazel-${PKG_VERSION}
 pushd src/tools/singlejar
@@ -32,6 +31,7 @@ pushd src/tools/singlejar
 	--subcommands \
 	--verbose_failures \
 	--noincompatible_enable_proto_toolchain_resolution \
+	--define=PROTOC_PREFIX=${BUILD_PREFIX} \
 	--define=PROTOBUF_INCLUDE_PATH=${PREFIX}/include \
 	--extra_toolchains=//bazel_toolchain:cc_cf_toolchain \
 	--extra_toolchains=//bazel_toolchain:cc_cf_host_toolchain \
@@ -42,3 +42,6 @@ pushd src/tools/singlejar
 mkdir -p $PREFIX/bin
 cp ../../../bazel-out/${TARGET_CPU}-fastbuild/bin/src/tools/singlejar/singlejar $PREFIX/bin
 cp ../../../bazel-out/${TARGET_CPU}-fastbuild/bin/src/tools/singlejar/singlejar_local $PREFIX/bin
+
+
+../../../bazel-${PKG_VERSION} clean --expunge
